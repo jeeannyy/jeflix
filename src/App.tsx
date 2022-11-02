ca6a6cf5558e9a6e45361b904ec28bcbe7b9706b
Jeeann <jeeann.kim@gmail.com>
2022-11-02 14:58:16 +0000

    finish header

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,14 @@ import Search from "./Routes/Search";
 import Tv from "./Routes/Tv";
 
 function App() {
-  return null;
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/tv">
-          <Tv />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-      </Routes>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tv" element={<Tv />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
     </Router>
   );
 }
