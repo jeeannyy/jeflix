fb43796337ddece5dc1a73408673cb7ee39ebeba
Jeeann <jeeann.kim@gmail.com>
2022-11-23 14:10:43 +0000

    edit package json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Routes/Home";
 import Search from "./Routes/Search";
